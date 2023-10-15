509bca3b9216fc78845d0595307284bf59127867
Júlio Caria <julio.caria@hotmail.com>
2023-10-15 17:30:12 -0300

    SpringBoot DevTools e Tratamento de Erros

--- src/main/java/br/com/juliocaria/todolist/task/TaskModel.java
+++ src/main/java/br/com/juliocaria/todolist/task/TaskModel.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity(name="tb_tasks")
+@Entity(name = "tb_tasks")
 public class TaskModel {
   /**
    * ID
@@ -24,21 +24,27 @@ public class TaskModel {
    * Prioridade
    */
 
-   @Id
-   @GeneratedValue(generator = "UUID")
-   private UUID id;
-   private String description;
-   
-   @Column(length = 50)
-   private String title;
-   private LocalDateTime startAt;
-   private LocalDateTime endAt;
-   private String priority;
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  private String description;
 
+  @Column(length = 50)
+  private String title;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
 
-   private UUID idUser;
-   
-   @CreationTimestamp
-   private LocalDateTime createdAt;
-   
+  private UUID idUser;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  public void setTitle(String title) throws Exception {
+    if(title.length() > 50) {
+      throw new Exception("O campo title deve conter no máximo 50 caracteres.");
+    }
+    this.title = title;
+  }
+
 }
