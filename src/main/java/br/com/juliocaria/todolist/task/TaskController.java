9da9d48bbb84be5a502c510dfc59fe4733c08b6f
Júlio Caria <julio.caria@hotmail.com>
2023-10-15 14:34:38 -0300

    Alteração Completa e Parcial das Tasks

--- src/main/java/br/com/juliocaria/todolist/task/TaskController.java
+++ src/main/java/br/com/juliocaria/todolist/task/TaskController.java
@@ -12,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.juliocaria.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/tasks")
@@ -48,5 +51,14 @@ public class TaskController {
     var idUser = request.getAttribute("idUser");
     var tasks = this.taskRepository.findByIdUser((UUID) idUser);
     return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
